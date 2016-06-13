3f404d0815b9a870f0ddb3315c52474da3951769
Simon Martin <smartin@scottlogic.co.uk>
2016-06-13 14:58:24 +0100

    recursion

--- ls.js
+++ ls.js
@@ -3,6 +3,7 @@
 var statvfs = require('./statvfs')
 var args = require('cli.args')()
 var fs = require('fs');
+var path = require('path')
 
 function stat(filename) {
     return new Promise( (resolve, reject) => {
@@ -15,13 +16,38 @@ function stat(filename) {
     })
 }
 
-const promises = args.nonOpt.map( (filename) => {
-    return Promise.resolve({filename})
-    .then( (r) => statvfs(filename).then( (vfs) => Object.assign({}, r, {vfs}) ) )
-    .then( (vfs) => stat(filename).then( (stat) => Object.assign({}, vfs, {stat}) ) )
-    //.then( (result) => (console.log(result), result))
-    .catch( (err) => console.error(err) )
-})
+function get_traits( stat ) {
+    return ['File','Directory','BlockDevice','CharacterDevice','SymbolicLink','FIFO','Socket']
+    .reduce( (traits, trait) => (traits[trait] = stat[`is${trait}`](),traits), {})
+}
+
+function map( filestat) {
+    console.log(filestat.filename)
+    return { [filestat.filename]: true}
+}
+
+function handle_single_path( pathname ) {
+    return new Promise( (resolve, reject) => {
+        fs.readdir( pathname, (err, entries) => {
+            if(err) {
+                return reject(err)
+            }
+            const promises = entries.map( (entry) => {
+                const filename = path.join( pathname, entry)
+                return Promise.resolve({filename})
+                .then( (r) => statvfs(filename).then( (vfs) => Object.assign({}, r, {vfs}) ) )
+                .then( (vfs) => stat(filename).then( (stat) => Object.assign({}, vfs, {stat} ) ) )
+                .then( (results) => Object.assign({}, results, {traits: get_traits(results.stat)}) )
+                .then( (results) => (console.log(results),results))
+                .then( (results) => results.stat.isDirectory() ? handle_single_path( results.filename ).then(()=>results) : results)
+                .then( map )
+            })
+            return resolve(Promise.all(promises))
+        })
+    })
+}
+
+const promises = args.nonOpt.map( (filename) => handle_single_path(filename) )
 
 Promise.all(promises)
 .then( (results) => {
