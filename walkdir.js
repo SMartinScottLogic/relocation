69de4e9c9b5c35e4dfcc29994090767759eab902
Simon Martin <smartin@scottlogic.co.uk>
2016-06-15 08:32:30 +0100

    ESLint config, wait for completion

--- walkdir.js
+++ walkdir.js
@@ -1,4 +1,5 @@
 'use strict'
+/* eslint-disable no-console */
 
 var walk = require('walkdir')
 var args = require('cli.args')()
@@ -10,7 +11,6 @@ function update_stats(root, filename, stat) {
 
     console.log('file', root, filename, size)
 
-    let done = false
     const parsed = path.parse(filename)
     path.relative(root, parsed.dir).split(path.sep).reduce((prev, cur) => {
         if (prev.length === 0) {
@@ -19,26 +19,20 @@ function update_stats(root, filename, stat) {
         if(cur.length === 0) return prev
         const tail = prev.slice(-1)[0]
         prev.push(path.join(tail, cur))
-        if (prev.length > 2) done = true
         return prev
     }, []).forEach((name) => (console.log(name), space[name] = (space[name] || 0) + size))
-
-    if (done) {
-        console.log(require('util').inspect(space, { depth: null }))
-        process.exit(-1)
-    }
 }
 
 function handle_single_path(root, pathname) {
     let files = 0
     return new Promise((resolve, reject) => {
-        var emitter = walk(pathname);
+        var emitter = walk(pathname)
 
         emitter.on('file', function onFile(filename, stat) {
             //console.log('file from emitter: ', filename);
             update_stats(root, filename, stat)
             files++
-        });
+        })
         emitter.on('error', function onError(path, err) {
             console.log('ERROR', new Date(), err, path)
         })
@@ -47,9 +41,11 @@ function handle_single_path(root, pathname) {
         })
         emitter.on('end', function onDone() {
             console.log('done', new Date(), `[found ${files} files]`)
-            resolve(true);
+            resolve(true)
         })
     })
 }
 
-args.nonOpt.map((filename) => handle_single_path(filename, filename))
+const promises = args.nonOpt.map((filename) => handle_single_path(filename, filename))
+Promise.all(promises)
+.then( () => console.log(require('util').inspect(space, { depth: null })) )
