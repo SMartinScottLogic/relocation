a7f19b8f4df84efdc57418a43f6e2ee181e22b32
Simon Martin <smartin@scottlogic.com>
2020-02-11 12:35:29 +0000

    Generate total size of each folder in tree

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,7 @@ struct SizedFile {
     size: u64
 }
 
-fn all_paths(base: &std::ffi::OsString, path: PathBuf) -> Vec<PathBuf> {
-    info!("{:?} {:?}", base, path);
-    info!("{:?}", path.strip_prefix(base).unwrap());
+fn all_paths(base: &std::ffi::OsString, path: &PathBuf) -> Vec<PathBuf> {
     path.strip_prefix(base).unwrap().iter().fold((Vec::new(), PathBuf::from("")), |mut acc, component| {
         acc.1.push(component);
         acc.0.push(acc.1.clone());
@@ -27,7 +25,7 @@ fn all_paths(base: &std::ffi::OsString, path: PathBuf) -> Vec<PathBuf> {
     }).0
 }
 
-fn find_files(sourceroot: &std::ffi::OsString) -> Vec<Vec<SizedFile>> {
+fn find_files(sourceroot: &std::ffi::OsString) -> BTreeMap<PathBuf, u64> {
     info!("find all files in {:?}.", sourceroot);
 
     let walk = WalkDir::new(sourceroot).into_iter();
@@ -36,11 +34,25 @@ fn find_files(sourceroot: &std::ffi::OsString) -> Vec<Vec<SizedFile>> {
         .map(|entry| entry.unwrap())
         .filter(|entry| entry.path().is_file())
         .map(|entry| (entry.metadata().unwrap().len(), entry.path().to_str().unwrap().to_string()))
+        .filter(|(size, _filename)| size > &0u64)
+        .map(|(size, filename)| (size, all_paths(sourceroot, &PathBuf::from(filename).parent().unwrap().to_path_buf())))
         .fold(BTreeMap::new(), |mut acc, entry| {
             let size = entry.0;
-            acc.entry(size).or_insert_with(Vec::new).push(SizedFile {filename: entry.1, size});
+            let paths = entry.1;
+            for path in paths {
+            *acc.entry(path).or_insert(0) += size;
+            }
             acc
-        }).values().cloned().collect()
+        })
+        /*
+        .flat_map(|(size, filename)| all_paths(sourceroot, &PathBuf::from(filename)).iter().map(move |path| (size, path)))
+        .fold(BTreeMap::new(), |mut acc, entry| {
+            let size = entry.0;
+            let path = entry.1;
+            *acc.entry(std::ffi::OsString::from(path)).or_insert(0) += size;
+            acc
+        })
+        */
 }
 
 fn main() {
@@ -58,12 +70,10 @@ fn main() {
         .init();
     // TODO cmd-line args
 
-    let sourceroot = env::args_os().nth(1).unwrap();
+    let sourceroot = env::args_os().nth(1).unwrap_or_else(|| std::ffi::OsString::from("."));
     let groups = find_files(&sourceroot);
 
     info!("result: {:#?}", groups);
-
-    info!("test: {:#?}", all_paths(&std::ffi::OsString::from("/workspace"), PathBuf::from("/workspace/relocation/.target/test/1234")))
 }
 
 #[cfg(test)]
@@ -75,7 +85,7 @@ mod tests {
         assert_eq!(
             super::all_paths(
                 &std::ffi::OsString::from("/workspace"), 
-                PathBuf::from("/workspace/relocation/.target/test/1234")
+                &PathBuf::from("/workspace/relocation/.target/test/1234")
             ), 
             vec!["relocation", "relocation/.target", "relocation/.target/test", "relocation/.target/test/1234"]
             .iter().map(PathBuf::from).collect::<Vec<PathBuf>>());
