405d7d33bdec1bf91ea63725a69b20cee392a9db
Simon Martin <smartin@scottlogic.com>
2020-03-10 08:45:38 +0000

    Recursive directory listing (rust)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::os::unix::fs::MetadataExt;
+use walkdir::WalkDir;
+
+fn main() -> Result<(), std::io::Error> {
+    let walker = WalkDir::new(".").into_iter();
+    for entry in walker {
+        let entry = entry?;
+        let meta = fs::metadata(entry.path())?;
+        let dev_id = meta.dev();
+        println!(
+            "{} {} {:o} {:?} {} {}",
+            dev_id,
+            entry.path().display(),
+            entry.metadata()?.mode(),
+            entry.metadata()?.is_dir(),
+            entry.metadata()?.is_file(),
+            entry.metadata()?.size()
+        );
+    }
+    Ok(())
 }
